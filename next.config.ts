ccae384786310c3e492cc6f4859f7294bc65b070
Sachin Kumar <sachinrajputlive@gmail.com>
2025-01-15 01:30:13 +0530

    [sprint-1] fixes

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'out',
   basePath: process.env.NODE_ENV === 'production' ? '/quick-learning-app' : '', // Use basePath in production
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/quick-learning-app' : '', // Serve static assets from base path
+  assetPrefix: '', // Serve static assets from base path
 };
 
 export default nextConfig;
