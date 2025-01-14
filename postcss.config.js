dfca517fea63cf5d497de884d1814be7f202c05d
Sachin Kumar <sachinrajputlive@gmail.com>
2025-01-14 22:53:14 +0530

    [sprint-1] fixes

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js
+module.exports = {
+    plugins: [
+      'tailwindcss',  // You can use a string to reference the plugin
+      'autoprefixer',  // Another example
+    ],
+  }
+  
