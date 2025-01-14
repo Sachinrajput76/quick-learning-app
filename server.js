775e944f35b140e5f82769cef6ce41c28eddfbae
Sachin Kumar <sachinrajputlive@gmail.com>
2025-01-15 00:29:00 +0530

    [sprint-1] fixes

--- server.js
+++ server.js
@@ -9,15 +9,20 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  // Serve static files
-  server.use('/_next', express.static(path.join(__dirname, '.next', '_next')));
+  // Serve static files for production with the correct basePath
+  const basePath = '/quick-learning-app'; // Adjust this based on your basePath configuration in next.config.js
 
+  // In production, we need to serve assets with the basePath prefix
+  server.use(`${basePath}/_next`, express.static(path.join(__dirname, '.next', '_next')));
+  
+  // Handle all requests
   server.all('*', (req, res) => {
     return handle(req, res);
   });
 
+  // Start the server on port 3000
   server.listen(3000, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log(`> Ready on http://localhost:3000${basePath}`);
   });
 });
