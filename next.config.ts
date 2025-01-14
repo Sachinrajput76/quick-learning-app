3c8c362f333918d8c2a461419802a97df3544d1e
Sachin Kumar <sachinrajputlive@gmail.com>
2025-01-14 22:35:40 +0530

    [sprint-1] app name fixes

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   distDir: 'out',
-  basePath: process.env.NODE_ENV === 'production' ? '/quick_learning_app' : '', // Use basePath in production
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/quick_learning_app' : '', // Serve static assets from base path
+  basePath: process.env.NODE_ENV === 'production' ? '/quick-learning-app' : '', // Use basePath in production
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/quick-learning-app' : '', // Serve static assets from base path
 };
 
 export default nextConfig;
