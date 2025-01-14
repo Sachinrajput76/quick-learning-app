0434c1dbc454fc72eca60e81326e983677d3e806
Sachin Kumar <sachinrajputlive@gmail.com>
2025-01-14 18:52:13 +0530

    [sprint-1] removed slash in the base path /

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   distDir: 'out',
-  basePath: '/quick-learning-app/',
-  assetPrefix: '/quick-learning-app/',
+  basePath: '/quick-learning-app',
+  assetPrefix: '/quick-learning-app',
   trailingSlash: true // Optional, but helps with GitHub Pages
 };
 
