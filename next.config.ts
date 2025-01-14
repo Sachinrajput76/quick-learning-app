36b91650eb61949d3ae9d44d58bde9d567ea3c12
Sachin Kumar <sachinrajputlive@gmail.com>
2025-01-14 18:30:57 +0530

    [sprint-1] added assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   distDir: 'out',
-  basePath: '/quick-learning-app'
+  basePath: '/quick-learning-app',
+  assetPrefix: '/quick-learning-app/'
 };
 
 export default nextConfig;
