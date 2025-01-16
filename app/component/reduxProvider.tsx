a06f8d68062130a7c4ad0d682c3ab0ad79e3a4e5
Sachin Kumar <sachinrajputlive@gmail.com>
2025-01-16 13:32:07 +0530

    [sprint-1] import react in the reduxProvider.tsx

--- app/component/reduxProvider.tsx
+++ app/component/reduxProvider.tsx
@@ -1,5 +1,5 @@
 'use client'; // Ensure this is a Client Component
-
+import React from 'react';
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
 import { store } from '@/store/store';
