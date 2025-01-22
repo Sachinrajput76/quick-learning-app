bf7985bd5a5cff1a00d368aa68e7a21375e8f124
Sachin Kumar <sachinrajputlive@gmail.com>
2025-01-22 12:26:37 +0530

    [sprint-1] husky pre-commit fixes for js code

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -1,1 +1,9 @@
--e "#!/bin/sh\n. \"$(dirname \"$0\")/_/husky.sh\"\n\nnpm run lint" 
+const { execSync } = require('child_process');
+
+try {
+  // Run lint command
+  execSync('npm run lint', { stdio: 'inherit' });
+} catch (error) {
+  console.error('Linting failed. Please fix errors before committing.');
+  process.exit(1); // Exit with an error code to fail the commit
+}
